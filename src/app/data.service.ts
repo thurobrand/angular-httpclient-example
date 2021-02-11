b37f91b2ca801dd9e3d9bb8d2e409e909002f187
Jonathan Keen <thurobrand@gmail.com>
2021-02-10 21:51:11 -0500

    Added parameters parsing for pagination

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse,HttpParams } from '@angular/common/http';
 
 import {  throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError,tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  public first:  string="";
+  public last:  string="";
+  public next:  string="";
+  public prev:  string="";
 
   private REST_API_SERVER = "http://localhost:3000/products";
 
@@ -28,6 +33,26 @@ export class DataService {
   }
 
   public sendGetRequest(){
-    return this.httpClient.get(this.REST_API_SERVER).pipe(retry(3), catchError(this.handleError));
+    const options = {params: new HttpParams({fromString: "_page=1&_limit=20"})};
+    return this.httpClient.get(this.REST_API_SERVER,options).pipe(retry(3), catchError(this.handleError));
+  }
+
+  public parseLinkHeader(header){
+    if(header.length==0){
+      return;
+    }
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach(p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name =section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name]=url;
+    });
+
+    this.first = links["first"];
+    this.last = links["last"];
+    this.prev = links["prev"];
+    this.next = links["next"];
   }
 }
